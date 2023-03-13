2ca81c9ff4aaa80117426d9ee82b111ed2670775
git <alenka180587@mail.ru>
2023-03-13 21:55:09 +0300

    Домашка

---
+++ Spiridonova_lesson_15.py
@@ -0,0 +1,35 @@
+
+# ДЗ № 15
+# 1. Создайте класс Person, который имеет атрибуты name и age, а также метод greet()
+# (выводит приветствие на экран с именем персоны).
+# class Person:
+#     name = None
+#     age = None
+# #
+# men = Person()
+# men.name = 'Юрий'
+# def greet(self,name,age):
+#        self.name = name
+#        self.age = age
+# print(f'Добро пожаловать! {men.name}')
+
+# 2. Создайте класс Car, который имеет атрибуты make, model и year, а также метод get_info()
+# (возвращает строку, содержащую информацию о машине).
+# class Car:
+#     make = None
+#     model = None
+#     year = None
+#
+# Mazda = Car()
+# Mazda.make = 'Америка'
+# Mazda.model = 'cx5'
+# Mazda.year = 2012
+# def get_data(self,make,model,year):
+#        self.make = make
+#        self.model = model
+#        self.year = year
+# def get_info(self):
+#  print('Информация о машине:')
+#  print(f'Сделана:', {self.make}, 'Модель:', {self.model}, 'Год выпуска:', {self.year})
+#
+# get_info(Mazda)
